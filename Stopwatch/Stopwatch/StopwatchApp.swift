234f0536ddeaf5d72b10e15287bbd5e7483aafab
seo-kh <gospelseokh@gmail.com>
2026-01-05 16:38:17 +0900

    add: project setting files

---
+++ Stopwatch/Stopwatch/StopwatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  StopwatchApp.swift
+//  Stopwatch
+//
+//  Created by alphacircle on 1/5/26.
+//
+
+import SwiftUI
+
+@main
+struct StopwatchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Stopwatch/StopwatchTests/StopwatchTests.swift
@@ -0,0 +1,16 @@
+//
+//  StopwatchTests.swift
+//  StopwatchTests
+//
+//  Created by alphacircle on 1/5/26.
+//
+
+import Testing
+
+struct StopwatchTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
