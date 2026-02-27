59d9983679bf0e6fd9746b1154547621b8a8aa25
seo-kh <gospelseokh@gmail.com>
2026-02-28 08:43:59 +0900

    feat: EmptyContent define, builder method added

---
+++ Stopwatch/WatchUI/Containers/EmptyContent.swift
@@ -0,0 +1,15 @@
+//
+//  EmptyContent.swift
+//  WatchUI
+//
+//  Created by 테스트 on 2/28/26.
+//
+
+import Foundation
+import SwiftUI
+
+public struct EmptyContent: WatchContent {
+    public func render(_ context: inout GraphicsContext, rect: CGRect) {
+        
+    }
+}
--- Stopwatch/WatchUI/WatchContentBuilder.swift
+++ Stopwatch/WatchUI/WatchContentBuilder.swift
@@ -9,15 +9,23 @@ import Foundation
 
 @resultBuilder
 public struct WatchContentBuilder {
-    public static func buildBlock<Content>(_ content: Content) -> some WatchContent where Content: WatchContent {
+    public static func buildBlock() -> EmptyContent {
+        return EmptyContent()
+    }
+    
+    public static func buildBlock<Content>(_ content: Content) -> Content where Content: WatchContent {
         return content
     }
     
+//    public static func buildBlock<each Content>(_ content: repeat each Content) -> TupleContent<(repeat each Content)> where repeat each Content: WatchContent {
+//        return TupleContent((repeat each content))
+//    }
+
     public static func buildBlock<each Content: WatchContent>(_ contents: repeat each Content) -> some WatchContent {
         return ArrayContent((repeat each contents))
     }
     
-    public static func buildExpression<Content>(_ content: Content) -> some WatchContent where Content: WatchContent {
+    public static func buildExpression<Content>(_ content: Content) -> Content where Content: WatchContent {
         return content
     }
 }
